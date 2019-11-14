98a30f37533a3c6c3ed06b292f7dad7057d02197
Ahmed Abdelhadi <aabdelh@cmtcde5u157.rd.corpintra.net>
2019-11-14 16:36:46 +0100

    Initial commit

---
+++ data_processing.py
@@ -0,0 +1,44 @@
+from talib import abstract
+import pandas as pd
+import numpy as np
+
+
+# instantiate TA-LIB functions
+CCI = abstract.Function('cci')
+MACD = abstract.Function('macd')
+SMA = abstract.Function('sma')
+RSI = abstract.Function('rsi')
+
+
+# import original dataset
+original_data = pd.read_excel(open('DAX_data.xlsx', 'rb'))
+
+# inputs should be provided in "OHLCV" format for the TA-LIB functions
+inputs = original_data[['Open', 'High', 'Low', 'Close', 'Volume']]
+inputs.columns = inputs.columns.str.lower()
+
+# CCI, Commodity Channel Index, timeperiod = the past 14 observations
+cci_output = CCI(inputs, timeperiod=14)
+
+#MACD, Moving Average Convergence/Diverence: fast period = 12, slow
+#period = 26, signal period = 9.
+macd_output = MACD(inputs, fastperiod=12, slowperiod=26, signalperiod=9)
+
+# Use the generated 'macdhist' data
+macd_output = macd_output['macdhist']
+
+# SMA, Simple Moving Average: timeperiod = the past 200 observations.
+sma_output = SMA(inputs, timeperiod=200)
+
+# RSI, Relative Strength Index: timeperiod = the past 14 observations.
+rsi_output = RSI(inputs, timeperiod=14)
+
+#final output: 5 features (Closing price + the 4 technical indicators) + drop NaN rows
+final_output = pd.concat([inputs['close'], cci_output, macd_output, sma_output, rsi_output], axis = 1).dropna()
+
+# save the final output as csv
+final_output.to_csv("final_output.csv", sep='\t', encoding='utf-8')
+
+# DONE! :)
+print("DONE")
+
