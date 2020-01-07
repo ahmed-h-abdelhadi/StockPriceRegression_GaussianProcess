21be6e2bf35baae317b7f33af9e34188686f884a
Ahmed Abdelhadi <ahmed.h.abdelhadi@gmail.com>
2020-01-07 14:27:07 +0100

    Adding plots and random search for features

--- data_processing_2.py
+++ data_processing_2.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns; sns.set()
 
-
-window_length = 2
+window_length = 1
 data = pd.read_csv("./data/final_output.csv", sep='\t', encoding='utf-8')
 
 final_dataset = np.zeros((data.shape[0] - window_length ,window_length*data.shape[1] + 1))
 final_dataset = np.zeros((data.shape[0] - window_length ,window_length*(data.shape[1] - 0) + 1))
+
+all_closing = []
+counter = 0
 # final_dataset = np.zeros((data.shape[0] - window_length , 6))
 for index, row in data.iterrows():
     if index <  data.shape[0] - window_length - 1:
@@ -20,6 +24,18 @@ for index, row in data.iterrows():
 
         datapoint_label = np.concatenate((datapoint, label))
         final_dataset[index] = datapoint_label.squeeze()
+
+        all_closing.append(row[0])
+        counter += 1
+
+x = range(counter)
+y1 = all_closing
+y2 = np.linspace(all_closing[0], all_closing[-1], counter)
+plt.plot(x, y1, "-b", label="data")
+plt.plot(x, y2, "--r", label="linear")
+plt.title("Closing Prices in Dataset")
+plt.legend(loc="upper left")
+plt.show()
 df = pd.DataFrame(final_dataset)
 
 df.to_csv("./data/final_dataset.csv", sep='\t', encoding='utf-8', index=False, header = False)
--- gp_main.py
+++ gp_main.py
@@ -1,31 +1,12 @@
 import GPy
 import pandas as pd
 import numpy as np
-
-# Our test grid
-[Xi, Xj] = np.meshgrid(np.linspace(-np.pi, np.pi, 50), np.linspace(-np.pi, np.pi, 50))
-
-# Number of samples [YOU CAN PLAY AROUND WITH THE NUMBER OF RANDOM SAMPLES TO SEE HOW THE FIT IS AFFECTED]
-num_measurements = 50
-f = lambda xi,xj: np.sin(xi) * np.sin(xj)
-# Random sample locations (2-D)
-X2 = np.random.uniform(-np.pi, np.pi, (num_measurements, 2))
-Y2 = np.array([f(x1,x2) for (x1,x2) in zip(X2[:,0], X2[:,1])])[:,None] + 0.05 * np.random.randn(X2.shape[0], 1)
-k = GPy.kern.RBF(2, active_dims=[0,1])
-
-# Create a GP Regression model with the sample locations and observations using the RBF kernel
-m = GPy.models.GPRegression(X2, Y2, k)
-
-# Optimise the kernel parameters
-m.optimize()
-
-Xnew2 = np.vstack((Xi.ravel(), Xj.ravel())).T
-
-mean2, Cov2 = m.predict(Xnew2)
+import random
+import matplotlib.pyplot as plt
 
 
-n_dimensions = 2 * 5
-n_training_points = 300
+n_dimensions = 1 * 5
+n_training_points = 120
 n_test_points = 300
 
 dataset = pd.read_csv("./data/final_dataset.csv", sep='\t', encoding='utf-8')
@@ -34,14 +15,63 @@ dataset = dataset.as_matrix()
 X = dataset[:n_training_points,:-1].reshape(-1, n_dimensions)
 Y = dataset[:n_training_points,-1].reshape(-1, 1)
 # Create a 2-D RBF kernel active over both input dimensions
-k = GPy.kern.RBF(n_dimensions, active_dims=range(n_dimensions))
 
-# Create a GP Regression model with the sample locations and observations using the RBF kernel
-m = GPy.models.GPRegression(X, Y, k)
 
-m.optimize()
+open('./RandomSearch.txt', 'w').close()
+with open("./RandomSearch.txt", "a") as file1:
+    for n in range(100):
+        print("Trial number: ", n)
 
-X_new = dataset[n_training_points: n_training_points + n_test_points,:-1].reshape(-1, n_dimensions)
-Y_new = dataset[n_training_points: n_training_points + n_test_points, -1].reshape(-1, 1)
-mean, cov = m.predict(X_new)
+        n_rbf_features = np.random.choice(range(3, n_dimensions+1))
+        n_std_periodic_features = np.random.choice(range(4, n_dimensions+1))
+        n_linear_features = np.random.choice(range(4, n_dimensions+1))
+        n_exponential_features = np.random.choice(range(2, n_dimensions+1))
+
+        rbf_features = sorted(random.sample(range(n_dimensions), n_rbf_features))
+        std_periodic_features = sorted(random.sample(range(n_dimensions), n_std_periodic_features))
+        linear_features = sorted(random.sample(range(n_dimensions), n_linear_features))
+        exponential_features =  sorted(random.sample(range(n_dimensions), n_exponential_features))
+
+        std_periodic_periodicity =  np.random.choice(range(19, 25))
+
+        k = GPy.kern.Linear(4, active_dims=[0, 1, 2, 3])
+
+        
+
+        try:
+            m = GPy.models.GPRegression(X, Y, k, normalizer = False)
+            m.optimize(optimizer='lbfgs', max_iters=1000)
+        except:
+            print("error!")
+            continue
+
+        X_new = dataset[n_training_points: n_training_points + n_test_points,:-1].reshape(-1, n_dimensions)
+        Y_new = dataset[n_training_points: n_training_points + n_test_points, -1].reshape(-1, 1)
+        mean, cov = m.predict(X_new)
+
+        error = np.sum(((mean - Y_new) ** 2)) / Y_new.shape[0]
+
+        file1.write("RBF features:" + str(rbf_features))
+        file1.write(" Linear features:" + str(linear_features))
+        # file1.write(" Std features:" + str( std_periodic_features))
+        # file1.write(" Std periodicity:" + str( std_periodic_periodicity))
+        # file1.write(" Exp features:" + str( exponential_features))
+        file1.write(" Error: "+  str(error))
+        file1.write("\n")
+
+        #m.plot(plot_density=True)
+        x = range(n_test_points)
+        y1 = Y_new
+        y2 = mean
+        plt.plot(x, y1, "-b", label="target")
+        plt.plot(x, y2, "-r", label="prediction")
+        plt.title("Performance of Linear Kernel Multivariate GP")
+        plt.legend(loc="upper left")
+        plt.show()
+
+        naive_prediction = sum((X_new[:,0]- Y_new.squeeze()) ** 2/Y_new.shape[0])
+
+        print("Error: ", error)
+
+# file1.close()
 print("DONE!")
